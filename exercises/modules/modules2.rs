89faad71fb381bac670dd92fa3b1bd61a9a0e6a0
langzi.me <wangyanqing@tftiancai.com>
2024-09-27 11:36:26 +0800

    Update modules2.rs

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -9,10 +9,9 @@
 
 // I AM NOT DONE
 
-mod delicious_snacks {
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+pub mod delicious_snacks {
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
