5d77daa9625f13e776a8f04970e140fa9b10605c
langzi.me <wangyanqing@tftiancai.com>
2024-09-27 12:11:49 +0800

    Update hashmaps3.rs

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -38,7 +38,22 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        // team_1
+        {
+            scores.entry(team_1_name).and_modify(|team| {
+                team.goals_scored += team_1_score;
+                team.goals_conceded += team_2_score;
+            }).or_insert(Team{ goals_scored: team_1_score, goals_conceded: team_2_score});
+
+        }
+        {
+            scores.entry(team_2_name).and_modify(|team| {
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            }).or_insert(Team{ goals_scored: team_2_score, goals_conceded: team_1_score});
+
+        }
+
     }
     scores
 }
