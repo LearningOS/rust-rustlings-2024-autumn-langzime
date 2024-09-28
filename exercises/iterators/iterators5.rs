63391375606281756a0899d6c7386af4c513be0c
langzi.me <wangyanqing@tftiancai.com>
2024-09-28 12:20:45 +0800

    Update iterators5.rs

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -35,7 +35,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.iter().filter(|x| x.1 == &value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +54,7 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    collection.into_iter().map(|x|x.iter()).flatten().filter(|x|x.1 == &value).count()
 }
 
 #[cfg(test)]
